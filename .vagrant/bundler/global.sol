1f6e77ac7a42217c1a23fd84101dbfb9b5a3108e
Matt Gibson <gothick@gothick.org.uk>
2020-11-08 15:32:36 +0000

    Initial Symfony project with Homestead

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostsupdater",["= 1.1.1.160"]],["vagrant-notify-forwarder",["= 0.5.0"]]],"checksum":"2eb03e09f9fa660a125160abd938dc4864881f754bd29a0e9a2f3b26b9dd5b11","vagrant_version":"2.2.10"}
