c8ad4b91b70531c1c6331bef5a8fd6d6d3b0656f
Matt Gibson <gothick@gothick.org.uk>
2020-11-10 19:56:17 +0000

    Use dates with time for my test form

--- src/Controller/WanderController.php
+++ src/Controller/WanderController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Wander;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -51,12 +52,12 @@ class WanderController extends AbstractController
         
         $form = $this->createFormBuilder($wander)
             ->add('title', TextType::class)
-            ->add('description', TextType::class)
-            ->add('startTime', DateType::class)
-            ->add('endTime', DateType::class)
+            ->add('description', TextareaType::class)
+            ->add('startTime', DateTimeType::class, ['' => ''])
+            ->add('endTime', DateTimeType::class)
             ->add('save', SubmitType::class, ['label' => 'Create Wander'])
             ->getForm();
-            
+
         return $this->render('wander/new.html.twig', [
             'form' => $form->createView(),
         ]);    
