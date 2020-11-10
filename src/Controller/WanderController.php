83cc6f66f629b3d4ddb68e36af01d945c4071afa
Matt Gibson <gothick@gothick.org.uk>
2020-11-10 19:45:55 +0000

    Simple HTML form demo

--- src/Controller/WanderController.php
+++ src/Controller/WanderController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Wander;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,7 +24,7 @@ class WanderController extends AbstractController
     }
 
     /**
-     * @Route("/wanders/testadd", name="create_wander")
+     * @Route("/wanders/testadd", name="testadd_wander")
      */
     public function createWander(): Response
     {
@@ -34,5 +37,28 @@ class WanderController extends AbstractController
         $entityManager->persist($wander);
         $entityManager->flush();
         return new Response("Saved a new wander with id " . $wander->getId());
+    }
+
+    /**
+     * @Route("/wanders/create", name="new_wander")
+     */
+    public function new(Request $request) {
+        $wander = new Wander();
+        $wander->setTitle("I'm a wander.");
+        $wander->setStartTime(new \DateTime());
+        $wander->setEndTime(new \DateTime());
+        $wander->setDescription("I'm a soulful description of a wander in Hotwells");
+        
+        $form = $this->createFormBuilder($wander)
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('startTime', DateType::class)
+            ->add('endTime', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Wander'])
+            ->getForm();
+            
+        return $this->render('wander/new.html.twig', [
+            'form' => $form->createView(),
+        ]);    
     }
 }
