6b3fb5ecfe5948d86c504ee4fdb19dec0654fc20
Matt Gibson <gothick@gothick.org.uk>
2020-11-11 23:13:08 +0000

    Add logout button

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,5 +22,14 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
     
 }
