be4d444872aca5fb0654bc558e12919d733ec920
Matt Gibson <gothick@gothick.org.uk>
2020-11-12 08:57:33 +0000

    Blundering around with asset decorator

---
+++ src/Serializer/ApiNormalizer.php
@@ -0,0 +1,60 @@
+<?php
+// api/src/Serializer/ApiNormalizer
+
+namespace App\Serializer;
+
+use ApiPlatform\Core\Api\UrlGeneratorInterface;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class ApiNormalizer implements NormalizerInterface, DenormalizerInterface, SerializerAwareInterface
+{
+    private $assetExtension;
+    private $router;
+
+    public function __construct(NormalizerInterface $decorated, AssetExtension $assetExtension)
+    {
+        if (!$decorated instanceof DenormalizerInterface) {
+            throw new \InvalidArgumentException(sprintf('The decorated normalizer must implement the %s.', DenormalizerInterface::class));
+        }
+
+        $this->decorated = $decorated;
+        $this->assetExtension = $assetExtension;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = $this->decorated->normalize($object, $format, $context);
+        if (is_array($data)) {
+            $data['date'] = date(\DateTime::RFC3339);
+            $data['gpxPath'] = $this->assetExtension->getAssetUrl('uploads/gpx/') . $data['gpxFilename'];
+        }
+
+        return $data;
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $this->decorated->supportsDenormalization($data, $type, $format);
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        return $this->decorated->denormalize($data, $class, $format, $context);
+    }
+
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        if($this->decorated instanceof SerializerAwareInterface) {
+            $this->decorated->setSerializer($serializer);
+        }
+    }
+}
