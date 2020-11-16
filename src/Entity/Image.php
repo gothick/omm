2f52937a7ac84bb811164cb9a3a2f64fa9faa0ec
Matt Gibson <gothick@gothick.org.uk>
2020-11-16 20:48:17 +0000

    Vich annotations added to Image and no apparent harm done :)

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 /**
  * @ApiResource()
@@ -18,6 +21,12 @@ class Image
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="image", fileNameProperty="name", size="sizeInBytes", 
+     *  mimeType="mimeType", originalName="originalName", dimensions="dimensions")
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -53,6 +62,33 @@ class Image
      * @ORM\Column(type="string", length=255)
      */
     private $dimensions;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * 
+     * @var \DateTimeInterface|null
+     */
+    private $updatedAt;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }    
 
     public function getId(): ?int
     {
