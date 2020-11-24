3c67292c31e1782ce10aa36cbea631aaf177a247
Matt Gibson <gothick@gothick.org.uk>
2020-11-24 21:29:00 +0000

    Add satellite layer and scale to map.

--- public/js/map.js
+++ public/js/map.js
@@ -1,31 +1,54 @@
+var streetMap, satelliteMap;
+
 $(function() {
     var mapbox_access_token = $('#mapid').data('mapboxAccessToken');
     var locus_radius = 1609.34; // 1 mile
     var base = L.latLng(51.4511364, -2.6219148);
-    var map = L.map('mapid', {
-        maxBounds: base.toBounds(locus_radius * 5) // Give a bit of wiggle room around the circle, but don't let the user drift too far away
-    }).setView(base, 14);
-
-    L.tileLayer(
-        // 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    
+    streetMap = L.tileLayer(
         'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=' + mapbox_access_token,
         {
-        // These are mapbox-specific
-        id: 'gothick/ckhb31na304g619t67r3gcngx',
-        tileSize: 512,
-        zoomOffset: -1,
-        // More general
-        maxZoom: 19,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-        // attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'        
-        }).addTo(map);
+            // These are mapbox-specific
+            id: 'gothick/ckhb31na304g619t67r3gcngx',
+            tileSize: 512,
+            zoomOffset: -1,
+            // More general
+            maxZoom: 19,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        });
+    
+    satelliteMap = L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=' + mapbox_access_token,
+        {
+            // These are mapbox-specific
+            id: 'gothick/ckhwgr59r0uai19o077hp87w4',
+            tileSize: 512,
+            zoomOffset: -1,
+            // More general
+            maxZoom: 19,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        });
+
     var circle = L.circle(base, {
         color: 'green',
         fillColor: '#faa',
         fillOpacity: 0.15,
         radius: locus_radius,
         interactive: false
-    }).addTo(map);
+    });
+    
+    var map = L.map('mapid', {
+        maxBounds: base.toBounds(locus_radius * 5), // Give a bit of wiggle room around the circle, but don't let the user drift too far away
+        layers: [satelliteMap, streetMap, circle]
+    }).setView(base, 14);
+    
+    var baseMaps = {
+        "Satellite": satelliteMap,
+        "Streets": streetMap
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+    L.control.scale().addTo(map);
 
     // Custom layer visually to set the most recent track
     var customLayer = L.geoJson(null, {
