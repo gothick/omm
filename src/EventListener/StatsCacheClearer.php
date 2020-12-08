b45c3359c0dc15edaa7128ddce9ef38dfb349387
Matt Gibson <gothick@gothick.org.uk>
2020-12-08 21:03:38 +0000

    Expand stats cache clearer into a Doctrine Lifecycle Subscriber so we can make sure to clear stats on all changes, including deletions

--- src/EventListener/StatsCacheClearer.php
+++ src/EventListener/StatsCacheClearer.php
@@ -4,25 +4,61 @@ namespace App\EventListener;
 
 use App\Entity\Image;
 use App\Entity\Wander;
+use Deployer\Logger\Logger;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
-class StatsCacheClearer
+class StatsCacheClearer implements EventSubscriber
 {
     /** @var TagAwareCacheInterface */
     private $cache;
 
-    public function __construct(TagAwareCacheInterface $cache)
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(
+            TagAwareCacheInterface $cache,
+            LoggerInterface $logger
+        )
     {
         $this->cache = $cache;    
+        $this->logger = $logger;
     }
 
-    // https://symfony.com/doc/current/doctrine/events.html#doctrine-lifecycle-listeners
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+            Events::postRemove,
+            Events::postUpdate
+        ];
+    }
+
     public function postPersist(LifecycleEventArgs $args): void
+    {
+        $this->clearCache($args);
+    }
+    public function postRemove(LifecycleEventArgs $args): void
+    {
+        $this->clearCache($args);
+    }
+    public function postUpdate(LifecycleEventArgs $args): void
+    {
+        $this->clearCache($args);
+    }
+    
+
+    // https://symfony.com/doc/current/doctrine/events.html#doctrine-lifecycle-listeners
+    public function clearCache(LifecycleEventArgs $args): void
     {
         $entity = $args->getObject();
         if ($entity instanceof Wander || $entity instanceof Image) {
+            $this->logger->debug('Clearing statistics cache on Doctrine lifecycle event');
             $this->cache->invalidateTags(['stats']);
         }
     }
+
 }
