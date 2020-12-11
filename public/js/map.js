cec8a886f82b551f30594d3494d0172c1b6c0f65
Matt Gibson <gothick@gothick.org.uk>
2020-12-11 17:52:41 +0000

    Fix up a few HTML and JS errors/lint warnings

--- public/js/map.js
+++ public/js/map.js
@@ -1,85 +1,58 @@
 // TODO: Refactor the heck out of this mess
 
-var streetMap, satelliteMap;
+var streetMap;
+var satelliteMap;
 var wgs84 = new GT_WGS84();
 var base = L.latLng(51.4511364, -2.6219148);
 
-function addDraggableCoordsMarker(map)
-{
-    return;
-    // https://gis.stackexchange.com/a/124288/967
-    var marker = L.marker(base, {
-        // TODO: Add autoPan etc.
-        draggable:true,
-    });
-    marker.bindPopup();
-
-    marker.on('dragend', function(event){
-        //alert('drag ended');
-        var marker = event.target;
-        var location = marker.getLatLng();
-        wgs84.setDegrees(location.lat, location.lng);
-        var popup = marker.getPopup();
-        var gridref = wgs84.getOSGB().getGridRef(5);
-        popup.setContent(
-            location.lat.toFixed(5) + ", " + 
-            location.lng.toFixed(5) + 
-            "<br>" +
-            gridref)
-        marker.openPopup();
-        });
-    marker.addTo(map);   
-}
-
 function setUpMap(options = {})
 {
-    var mapbox_access_token = $('#mapid').data('mapboxAccessToken');
+    var mapbox_access_token = $("#mapid").data("mapboxAccessToken");
     var locus_radius = 1609.34; // 1 mile
 
-    
     streetMap = L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=' + mapbox_access_token,
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=" + mapbox_access_token,
         {
             // These are mapbox-specific
-            id: 'gothick/ckhb31na304g619t67r3gcngx',
+            id: "gothick/ckhb31na304g619t67r3gcngx",
             tileSize: 512,
             zoomOffset: -1,
             // More general
             maxZoom: 19,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>"
         });
-    
+
     satelliteMap = L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=' + mapbox_access_token,
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}@2x?access_token=" + mapbox_access_token,
         {
             // These are mapbox-specific
-            id: 'gothick/ckhwgr59r0uai19o077hp87w4',
+            id: "gothick/ckhwgr59r0uai19o077hp87w4",
             tileSize: 512,
             zoomOffset: -1,
             // More general
             maxZoom: 19,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>"
         });
 
     var circle = L.circle(base, {
-        color: 'green',
-        fillColor: '#faa',
+        color: "green",
+        fillColor: "#faa",
         fillOpacity: 0.15,
         radius: locus_radius,
         interactive: false
     });
-    
+
     options = Object.assign(
-        options, 
+        options,
         {
-            maxBounds: base.toBounds(locus_radius * 5), // Give a bit of wiggle room around the circle, but don't let the user drift too far away
+            maxBounds: base.toBounds(locus_radius * 5), // Give a bit of wiggle room around the circle, but don"t let the user drift too far away
             layers: [streetMap, circle]
         }
     );
 
-    var map = L.map('mapid', options)
+    var map = L.map("mapid", options)
         .setView(base, 14);
-    
+
     var baseMaps = {
         "Satellite": satelliteMap,
         "Streets": streetMap
@@ -90,41 +63,40 @@ function setUpMap(options = {})
 
     L.control.locate().addTo(map);
 
-    addDraggableCoordsMarker(map);
     return map;
 }
 
 function selectedWanderStyle() {
     return {
-        color: '#FFA500'
+        color: "#FFA500"
     };
 }
 
 function unselectedWanderStyle() {
     return {
-        color: '#3388FF'
+        color: "#3388FF"
     };
 }
 
 var currentlySelected = null;
 
 const CustomGeoJSON = L.GeoJSON.extend({
-    options: { 
+    options: {
         // TODO: Do we need to go to all this trouble to keep the wander id
         // handy? Maybe we could just capture it in the bindPopup closure?
-        wander_id: null,
+        wander_id: null
     }
  });
 
 function addAllWanders(map)
 {
-    // TODO: We should probably use some kind of Hydra client. This'll do for now.
+    // TODO: We should probably use some kind of Hydra client. This"ll do for now.
     $.getJSON("/api/wanders", function(data) {
-        var last = data['hydra:totalItems'];
-        $.each(data['hydra:member'], function(key, wander) {
+        var last = data["hydra:totalItems"];
+        $.each(data["hydra:member"], function(key, wander) {
             isLastWander = (last - 1 == key);
-            var track = omnivore.gpx(wander.gpxFilename, 
-                    null, 
+            var track = omnivore.gpx(wander.gpxFilename,
+                    null,
                     new CustomGeoJSON(null, {
                         wander_id: wander.id,
                         style: isLastWander ? selectedWanderStyle() : unselectedWanderStyle()
@@ -138,17 +110,17 @@ function addAllWanders(map)
 
                     addWanderImages(map, layer.options.wander_id);
                     // Popup
-                    var template = '<a href="{contentUrl}">{title}</a>';
+                    var template = "<a href='{contentUrl}'>{title}</a>";
                     return L.Util.template(template, wander);
                 })
             track.wander_id = wander.id;
-            
+
             track.addTo(map);
             if (isLastWander) {
                 currentlySelected = track;
             }
         });
-    });    
+    });
 }
 
 var photoLayer = null;
@@ -159,17 +131,17 @@ function addPhotos(map, photos)
         map.removeLayer(photoLayer);
     }
 
-    photoLayer = L.photo.cluster().on('click', function(evt) {
+    photoLayer = L.photo.cluster().on("click", function(evt) {
         var photo = evt.layer.photo;
-        var template = '<a href="{imageShowUri}"><img src="{url}" width="300" /></a><p>{caption}</p>';
+        var template = "<a href='{imageShowUri}'><img src='{url}' width='300' /></a><p>{caption}</p>";
         // TODO: Video
         evt.layer.bindPopup(L.Util.template(template, photo), {
-            className: 'leaflet-popup-photo',
+            className: "leaflet-popup-photo",
             minWidth: 300
         }).openPopup();
     });
 
-    photoLayer.add(photos).addTo(map);    
+    photoLayer.add(photos).addTo(map);
 }
 
 function addWanderImages(map, wander_id) {
@@ -177,12 +149,12 @@ function addWanderImages(map, wander_id) {
 
     // Our API allows us to grab only those photos with co-ordinates set
     $.getJSON("/api/wanders/" + wander_id + "/images?exists[latlng]=true", function(images) {
-        $.each(images['hydra:member'], function(key, image) {
+        $.each(images["hydra:member"], function(key, image) {
             photos.push({
                 lat: image.latlng[0],
                 lng: image.latlng[1],
                 url: image.mediumImageUri,
-                caption: image.title || '',
+                caption: image.title || "",
                 thumbnail: image.markerImageUri,
                 imageShowUri: image.imageShowUri,
                 // TODO?
@@ -196,13 +168,11 @@ function addWanderImages(map, wander_id) {
 function addWander(map, wander_id, add_images = false)
 {
     $.getJSON("/api/wanders/" + wander_id, function(wander) {
-    
         var track = omnivore.gpx(wander.gpxFilename)
             .bindPopup(function(layer) {
                 return wander.title;
             })
             .addTo(map);
-
         // Based on the example at https://github.com/turban/Leaflet.Photo/blob/gh-pages/examples/picasa.html
         if (add_images) {
             addWanderImages(map, wander_id);
