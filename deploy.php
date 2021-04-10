dff1ee139d3307c7e694291b60334ae1a934f043
Matt Gibson <gothick@gothick.org.uk>
2021-04-10 10:02:17 +0000

    Try to restart messenger workers after deploy

--- deploy.php
+++ deploy.php
@@ -12,9 +12,9 @@ set('application', 'omm.gothick.org.uk');
 set('repository', 'git@github.com:gothick/omm.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
+// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', [
     'public/uploads/gpx',
@@ -24,7 +24,7 @@ add('shared_dirs', [
     'php_external_tools_bin'
 ]);
 
-// Writable dirs by web server 
+// Writable dirs by web server
 add('writable_dirs', []);
 
 // Saves me typing it out every time
@@ -40,12 +40,17 @@ host('ssh.gothick.org.uk')
     ->setRemoteUser('omm')
     ->set('cachetool_args', '--fcgi=/run/php/chef-managed-fpm-omm.sock --tmp-dir=/tmp')
     ->set('console_options', '-vvv')
-    ->set('deploy_path', '/var/www/sites/gothick.org.uk/{{application}}');    
-    
+    ->set('deploy_path', '/var/www/sites/gothick.org.uk/{{application}}');
+
 // Tasks
 
 task('build', function () {
     run('cd {{release_path}} && build');
+});
+
+desc('Stop any existing messenger consumers; Supervisor will restart them.');
+task('deploy:stop-workers', function () {
+    run('{{bin/console}} messenger:stop-workers {{console_options}}');
 });
 
 // Testing
@@ -64,6 +69,14 @@ after('deploy:failed', 'deploy:unlock');
 
 // Clear opcache on successful deployment
 after('deploy:symlink', 'cachetool:clear:opcache');
+
+// Restart messenger consuers on successful deployment
+// I don't think this is currently working, but it's probably
+// because of this bug:
+// https://github.com/symfony/symfony/issues/40477
+// So for now I'm probably going to have to manually restart
+// stuff with Supervisor :(
+after('cachetool:clear:opcache', 'deploy:stop-workers');
 
 // Migrate database before symlink new release.
 
