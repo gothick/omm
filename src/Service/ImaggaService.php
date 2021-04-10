858ced96d50f53cbc5fe470d98c00a1056fae2c0
Matt Gibson <gothick@gothick.org.uk>
2021-04-10 06:54:31 +0000

    Add dev environment config for messenger and don't forget to turn the 'real' image back on in the message handler now we now it won't cause an API failure

--- src/Service/ImaggaService.php
+++ src/Service/ImaggaService.php
@@ -47,8 +47,10 @@ class ImaggaService
 
         $response = $this->guzzle->request('GET', '/v2/tags', [
             'query' => [
-                //'image_url' => $image->getMediumImageUri(),
-                'image_url' => 'https://omm.gothick.org.uk/media/cache/srcset_576/uploads/images/20210328-mg-9921-terry-house-6067172c952d1544478055.jpg',
+                'image_url' => $image->getMediumImageUri(),
+                // TODO: Find a better way of faking this in the dev environment
+                // Maybe just always tag the images with some test tags?
+                // 'image_url' => 'https://omm.gothick.org.uk/media/cache/srcset_576/uploads/images/20210328-mg-9921-terry-house-6067172c952d1544478055.jpg',
                 'threshold' => 15.0
             ]
         ]);
