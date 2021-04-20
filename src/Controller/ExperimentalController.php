58156e312afab98651f6944a65a21ea47a1ec6d1
Matt Gibson <gothick@gothick.org.uk>
2021-04-20 20:45:45 +0000

    Image clustering experiment goes public

---
+++ src/Controller/ExperimentalController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use SebastianBergmann\CodeCoverage\Report\Xml\Report;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/experimental", name="experimental_")
+ */
+class ExperimentalController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('experimental/index.html.twig');
+    }
+    /**
+     * @Route("/imagecluster", name="imagecluster", methods={"GET"})
+     */
+    public function cluster(): Response
+    {
+        return $this->render('experimental/imagecluster.html.twig', [
+        ]);
+    }
+}
+
