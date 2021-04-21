5953b1ed1845ce1aeab25b406edb13b2a06640fc
Matt Gibson <gothick@gothick.org.uk>
2021-04-21 21:59:03 +0000

    Remove redirection now we've fixed up all existing image links, fixing #31

--- src/Controller/Image/ImageController.php
+++ src/Controller/Image/ImageController.php
@@ -8,10 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/image", name="image_")
+ */
 class ImageController extends AbstractController
 {
     /**
-     * @Route("/image/{id}", name="image_show", methods={"GET"})
+     * @Route("/{id}", name="show", methods={"GET"})
      */
     public function show(Image $image, ImageRepository $imageRepository): Response
     {
@@ -22,20 +25,5 @@ class ImageController extends AbstractController
             'prev' => $prev,
             'next' => $next
         ]);
-    }
-
-    /**
-     * @Route("/images/{id}", name="old_images_show_redirect", methods={"GET"})
-     */
-    public function oldImagesShowRedirect(Image $image): Response
-    {
-        // We used to use /images/ instead of /image/ but changed it
-        // because that clashed with the dev firewall rule that didn't
-        // do any authentication for /images/ as it's also the
-        // /public/images directory.
-        // TODO: Go through and remove all the existing image links
-        // in the wander and image descriptions that point to
-        // /images/ and then remove this.
-        return $this->redirectToRoute('image_show', ['id' => $image->getId()], 302);
     }
 }
