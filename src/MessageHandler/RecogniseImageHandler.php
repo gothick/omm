f83f1cca5971680e732bd691c105ccd309bb5d58
Matt Gibson <gothick@gothick.org.uk>
2021-05-04 22:27:32 +0100

    Add some debugging to see if we can figure out why image recognition messages seem to be going astray sometimes.

--- src/MessageHandler/RecogniseImageHandler.php
+++ src/MessageHandler/RecogniseImageHandler.php
@@ -5,6 +5,7 @@ namespace App\MessageHandler;
 use App\Message\RecogniseImage;
 use App\Repository\ImageRepository;
 use App\Service\ImageTaggingServiceInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
@@ -16,18 +17,26 @@ class RecogniseImageHandler implements MessageHandlerInterface {
     /** @var ImageRepository */
     private $imageRepository;
 
+    /** @var LoggerInterface */
+    private $logger;
+
     public function __construct(
         ImageTaggingServiceInterface $imageTaggingService,
-        ImageRepository $imageRepository)
+        ImageRepository $imageRepository,
+        LoggerInterface $logger)
     {
         $this->imageTaggingService = $imageTaggingService;
         $this->imageRepository = $imageRepository;
+        $this->logger = $logger;
     }
 
     public function __invoke(RecogniseImage $recogniseImage): void
     {
+        $this->logger->debug('RecogniseImageHandler invoked');
         $image = $this->imageRepository->find($recogniseImage->getImageId());
+        $this->logger->debug("RecogniseImageHandler found image with id " . $image->getId());
         if ($image !== null) {
+            $this->logger->debug("RecogniseImageHandler calling image tagger");
             $this->imageTaggingService->tagImage($image, $recogniseImage->getOverwrite());
         }
     }
