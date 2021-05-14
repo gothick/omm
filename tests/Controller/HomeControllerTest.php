c6b2e34468cdf3ad878d9fc8e71d4551d6bd0204
Matt Gibson <gothick@gothick.org.uk>
2021-05-14 20:18:52 +0000

    Add first basic functional test

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest Extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+    }
+}
