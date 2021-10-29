0dbc0ef64a462fbab6b71effea75d0262f753c81
Matt Gibson <gothick@gothick.org.uk>
2021-10-29 21:01:06 +0000

    Remove last couple of security-related deprecation warnings

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,14 +5,14 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-    /** @var UserPasswordEncoderInterface */
+    /** @var UserPasswordHasherInterface */
     private $userPasswordEncoder;
 
-    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    public function __construct(UserPasswordHasherInterface $userPasswordEncoder)
     {
         $this->userPasswordEncoder = $userPasswordEncoder;
     }
@@ -21,7 +21,7 @@ class UserFixtures extends Fixture
     {
         $user = new User();
         $user->setUsername('admin');
-        $user->setPassword($this->userPasswordEncoder->encodePassword($user, 'password123'));
+        $user->setPassword($this->userPasswordEncoder->hashPassword($user, 'password123'));
         $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
         $manager->flush();
