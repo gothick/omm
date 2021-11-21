7ffc516f7abf337ba8edd4e169dd454eeef3cb15
Matt Gibson <gothick@gothick.org.uk>
2021-11-21 15:59:43 +0000

    Got Masonry basics working.

--- assets/wanderpage.js
+++ assets/wanderpage.js
@@ -4,9 +4,17 @@ const $ = require('jquery');
 import { setUpMap } from './map.js';
 import { addWander } from './map.js';
 
+const Masonry = require('masonry-layout');
+
+
 $(function() {
     var map = setUpMap({
         scrollWheelZoom: false
     });
     addWander(map, $('#mapid').data('wanderId'), true);
+
+    var msnry = new Masonry( '.gallery', {
+        itemSelector: '.grid-item'
+    });
 });
+
