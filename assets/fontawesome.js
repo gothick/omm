1daeed390887a33ce105af6b04048638c86248e5
Matt Gibson <gothick@gothick.org.uk>
2021-11-27 20:34:57 +0000

    Switch to JS/SVG FontAwesome with tree shaking

---
+++ assets/fontawesome.js
@@ -0,0 +1,7 @@
+// https://fontawesome.com/v5.15/how-to-use/on-the-web/advanced/svg-javascript-core
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+// The Sort icons are for knpPaginator's bootstrap_v5_fa_sortable_link.html.twig
+import { faPlus, faMapMarker, faInfoCircle, faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus, faMapMarker, faInfoCircle, faSort, faSortUp, faSortDown);
+dom.watch();
