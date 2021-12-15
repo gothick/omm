694e473a0dabbc6dcfb43365ff1bc4e753b7c377
Matt Gibson <gothick@gothick.org.uk>
2021-12-15 21:14:30 +0000

    Enable some basic caching on production; should at least cache our most annoying response, which is the Wander API results

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Kernel;
+use App\CacheKernel;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +25,11 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+if ('prod' === $kernel->getEnvironment() ||
+    'staging' === $kernel->getEnvironment())
+{
+    $kernel = new CacheKernel($kernel);
+}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
