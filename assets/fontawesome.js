acfdfcfbb92f6682410640f72e885dd481c117db
Matt Gibson <gothick@gothick.org.uk>
2021-12-18 23:13:08 +0000

    eslint warnings fixed.

--- assets/fontawesome.js
+++ assets/fontawesome.js
@@ -1,26 +1,26 @@
 // https://fontawesome.com/v5.15/how-to-use/on-the-web/advanced/svg-javascript-core
 import {
-    library,
-    dom
+  library,
+  dom,
 } from '@fortawesome/fontawesome-svg-core';
 // The Sort icons are for knpPaginator's bootstrap_v5_fa_sortable_link.html.twig
 import {
-    faPlus,
-    faMapMarker,
-    faInfoCircle,
-    faExclamationCircle,
-    faSort,
-    faSortUp,
-    faSortDown
+  faPlus,
+  faMapMarker,
+  faInfoCircle,
+  faExclamationCircle,
+  faSort,
+  faSortUp,
+  faSortDown,
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
-    faPlus,
-    faMapMarker,
-    faInfoCircle,
-    faExclamationCircle,
-    faSort,
-    faSortUp,
-    faSortDown
+  faPlus,
+  faMapMarker,
+  faInfoCircle,
+  faExclamationCircle,
+  faSort,
+  faSortUp,
+  faSortDown,
 );
 dom.watch();
