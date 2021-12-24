06fe6f56c50f7a6f20f2d9520b85216035ff4c26
Matt Gibson <gothick@gothick.org.uk>
2021-12-24 14:18:13 +0000

    ESLint auto-fixes.

--- assets/imageclusterpage.js
+++ assets/imageclusterpage.js
@@ -1,32 +1,31 @@
+import { setUpMap, addPhotos } from './map.js';
+
 const $ = require('jquery');
 
-import { setUpMap } from './map.js';
-import { addPhotos } from './map.js';
+$(() => {
+  const map = setUpMap({
+    scrollWheelZoom: false,
+  });
 
-$(function() {
-    var map = setUpMap({
-        scrollWheelZoom: false
-    });
-
-    map.fireEvent('dataloading');
-    $.getJSON("/api/images", function(images) {
-        var photos = [];
-        for (var image of images) {
-            photos.push({
-                lat: image.latlng[0],
-                lng: image.latlng[1],
-                url: image.mediumImageUri,
-                caption: image.title || '',
-                thumbnail: image.markerImageUri,
-                imageShowUri: image.imageShowUri,
-                // imageEntityAdminUri: image.imageEntityAdminUri,
-                // TODO?
-                video: null
-            });
-        };
-        addPhotos(map, photos);
-    })
-    .always(function() {
-        map.fireEvent("dataload");
+  map.fireEvent('dataloading');
+  $.getJSON('/api/images', (images) => {
+    const photos = [];
+    for (const image of images) {
+      photos.push({
+        lat: image.latlng[0],
+        lng: image.latlng[1],
+        url: image.mediumImageUri,
+        caption: image.title || '',
+        thumbnail: image.markerImageUri,
+        imageShowUri: image.imageShowUri,
+        // imageEntityAdminUri: image.imageEntityAdminUri,
+        // TODO?
+        video: null,
+      });
+    }
+    addPhotos(map, photos);
+  })
+    .always(() => {
+      map.fireEvent('dataload');
     });
 });
