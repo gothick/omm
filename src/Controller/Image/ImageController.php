4cca02f23bee3d80d9f716da2c272fe87151772a
Matt Gibson <gothick@gothick.org.uk>
2021-12-27 12:50:57 +0000

    First steps toward parameter form on image index page.

--- src/Controller/Image/ImageController.php
+++ src/Controller/Image/ImageController.php
@@ -42,10 +42,27 @@ class ImageController extends AbstractController
         PaginatorInterface $paginator
     ): Response {
 
+        $year = intval($request->query->get('year', 0));
+        if ($year < 1900 || $year > 5000) {
+            $year = null;
+        }
+
+        $month = $request->query->getInt('month', 0);
+        if ($month < 1 || $month > 12) {
+            $month = null;
+        }
+
+        $rating = $request->query->getInt('rating', -1);
+        if ($rating === -1) {
+            $rating = null;
+        }
+
+        $ratingCompare = (string) $request->query->get('rating_compare', 'eq');
+
         $qb = $imageRepository->getReversePaginatorQueryBuilder();
 
-        $this->filterQueryByYearAndMonth($request->query, $qb);
-        $this->filterQueryByRating($request->query, $qb);
+        $this->filterQueryByYearAndMonth($year, $month, $qb);
+        $this->filterQueryByRating($rating, $ratingCompare, $qb);
 
         if ($request->query->has('location')) {
             $qb->andWhere('i.location = :location')
@@ -66,16 +83,14 @@ class ImageController extends AbstractController
         ]);
     }
 
-    private function filterQueryByYearAndMonth(InputBag $params, QueryBuilder &$qb): QueryBuilder
+    private function filterQueryByYearAndMonth(?int $year, ?int $month, QueryBuilder &$qb): QueryBuilder
     {
         // Our year parameter can be entirely missing, or "any", as well as an
         // integer year.
-        $year = intval($params->get('year', -1));
-        if ($year > 1900 && $year < 5000) { // Don't want to send any implausible years to MySQL
+        if ($year !== null) { // Don't want to send any implausible years to MySQL
             // We can optionally have a month to limit the date range
             // even further.
-            $month = $params->getInt('month', 0);
-            if ($month < 1 || $month > 12) {
+            if ($month === null) {
                 // Just the year
                 $startDate = Carbon::create($year, 1, 1);
                 $endDate = Carbon::create($year + 1, 1, 1);
@@ -93,11 +108,9 @@ class ImageController extends AbstractController
         return $qb;
     }
 
-    private function filterQueryByRating(InputBag $params, QueryBuilder &$qb): QueryBuilder
+    private function filterQueryByRating(?int $rating, string $ratingCompare, QueryBuilder &$qb): QueryBuilder
     {
-        $rating = $params->getInt('rating', -1);
-        if ($rating >= 0 && $rating <= 5) {
-            $ratingCompare = $params->get('rating_compare', 'eq');
+        if ($rating !== null) {
             switch ($ratingCompare) {
                 case 'lte':
                     $qb->andWhere($qb->expr()->lte('i.rating', ':rating'));
