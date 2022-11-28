0db761e63c1e25992bb0908bb3d27101393f0f14
Matt Gibson <gothick@gothick.org.uk>
2022-11-28 22:24:39 +0000

    Attempting to get image uploads working with Google Vision image tagging, plus persist image files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 ./wait-for-it.sh docker-database:3306 -t 30
 php ./bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
+# Start messenger consumer in background
+nohup php ./bin/console messenger:consume async -vv --time-limit=3600 2>&1 &
 # start apache
 apache2-foreground
