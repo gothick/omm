90bfacd9bddd5c57fc60320a043897cd1b6c3baa
Matt Gibson <gothick@gothick.org.uk>
2022-12-04 19:09:32 +0000

    Increase elasticsearch timeout and naughtily, in the same commit, don't bother running the Messenger consumer if we're in the dev environment, which has Messenger configured for synchronous running so doesn't have a real queue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 ./wait-for-it.sh docker-database:3306 -t 30 && \
-	./wait-for-it.sh elasticsearch:9200 -t 60
+	./wait-for-it.sh elasticsearch:9200 -t 120 # Elasticsearch can be very slow to get started
 php ./bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 # Start messenger consumer in background
-# TODO: This isn't needed in the dev environment as the tranport mechanism
+# This isn't needed in the dev environment as the transport mechanism
 # is synchronous, so the messages are never sent and there's no queue to
 # consume.
-nohup php ./bin/console messenger:consume async -vv --time-limit=3600 2>&1 &
+if [ "$APP_ENV" != "dev"]
+then
+	nohup php ./bin/console messenger:consume async -vv --time-limit=3600 2>&1 &
+fi
 # start apache
 apache2-foreground
