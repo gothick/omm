41e2b41a1bddcf856b9fb07736df07a25f200750
Matt Gibson <gothick@gothick.org.uk>
2024-02-03 22:24:14 +0000

    Upgrade phpunit-bridge recipe stuff

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
@@ -16,3 +14,7 @@ define('PHPEXIF_TEST_ROOT', __DIR__);
 // each time we test otherwise we'll end up caching test results from previous
 // runs.
 (new \Symfony\Component\Filesystem\Filesystem())->remove(__DIR__.'/../var/cache/test');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
