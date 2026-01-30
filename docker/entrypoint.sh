d84966d289ab183a9af33240212a54d529ad4552
Matt Gibson <gothick@gothick.org.uk>
2026-01-30 19:06:06 +0000

    Fix Docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,7 @@ RD_HOST="$(php ./parseurl.php --component=host --url=$REDIS_URL)"
 RD_PORT="$(php ./parseurl.php --component=port --url=$REDIS_URL)"
 
 ./wait-for-it.sh $DB_HOST:$DB_PORT -t 30 && \
-	./wait-for-it.sh $RD_HOST:$RD_PORT -t 30
+	./wait-for-it.sh $RD_HOST:$RD_PORT -t 30 \
 	./wait-for-it.sh $ES_HOST:$ES_PORT -t 120 # Elasticsearch can be very slow to get started
 php ./bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
