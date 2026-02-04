a97fc8497b526ac5547b13a77a41129e03c59fcd
Matt Gibson <gothick@gothick.org.uk>
2026-02-04 21:55:34 +0000

    Remove unused imports and tidy code in NeighbourhoodService

--- src/Service/NeighbourhoodService.php
+++ src/Service/NeighbourhoodService.php
@@ -2,15 +2,7 @@
 
 namespace App\Service;
 
-use App\Entity\Image;
-use App\Entity\Neighbourhood;
 use App\Repository\NeighbourhoodRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use PhpParser\Node\Expr\Cast\Double;
-use Psr\Log\LoggerInterface;
-use Spatie\GuzzleRateLimiterMiddleware\RateLimiterMiddleware;
 
 class NeighbourhoodService implements NeighbourhoodServiceInterface
 {
@@ -32,10 +24,6 @@ class NeighbourhoodService implements NeighbourhoodServiceInterface
             ->neighbourhoodRepository
             ->findByLatlng($lat, $lng);
 
-        if ($neighbourhood === null) {
-            return null;
-        } else {
-            return $neighbourhood->getLsoa11ln();
-        }
+        return $neighbourhood?->getLsoa11ln();
     }
 }
