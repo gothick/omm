d8d59b7a48f31d1e9c5cf2f0e322fed85ac4223a
Matt Gibson <gothick@gothick.org.uk>
2026-03-01 15:16:25 +0000

    Revert kernel load to standard Symfony 7.4 style load. We're not using a proper reverse proxy, just the inbuilt Symfony http cache, so we don't need the proxy config, and I've tested the app in dev and it seems to work fine without the specified umask, so I'm not even sure what that was for.

--- public/index.php
+++ public/index.php
@@ -1,28 +1,8 @@
 <?php
 
 use App\Kernel;
-use App\CacheKernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
